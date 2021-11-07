bdbffe3e2f35ef6cd92f299880206c1c147afdbc
Travis Engh <engh.travis@outlook.com>
2021-11-06 21:21:13 -0500

    New hamburger menu with no javascript

--- js/script.js
+++ js/script.js
@@ -14,12 +14,3 @@ function scrollFunction() {
     }
 }
 
-/*Toggle menu links with hamburger menu*/
-function myFunction() {
-    var x = document.getElementById('navbar-right');
-    if (x.style.display === 'block') {
-      x.style.display = 'none';
-    } else {
-      x.style.display = 'block';
-    }
-  } 
